31c650bc1ba6c77b646e7aa3a3692f3120d9b360
OHFloree <2001.florian.mueller@gmail.com>
2022-07-15 11:21:25 +0200

    tests for BacktrackingSolver

---
+++ src/service/solver/BacktrackingSolver/BacktrackingSolver.test.js
@@ -0,0 +1,44 @@
+const BacktrackingSolver = require("./BacktrackingSolver")
+const O = require("fp-ts/lib/Option")
+
+
+describe("Backtracking Solver", () => {
+    const solver = new BacktrackingSolver.default
+
+    it("should find a solution for a correct defined puzzle", () => {
+        let input = [
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9]
+        ]
+
+        let result = solver.solve(input)
+
+        expect(O.isSome(result)).toBe(true)
+    })
+
+    it("should not find a solution for an overconstraint puzzle", () => {
+        let input = [
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 1, 0, 8, 0, 0, 7, 9]
+        ]
+
+        let result = solver.solve(input)
+
+        expect(O.isSome(result)).toBe(false)
+    })
+
+})
