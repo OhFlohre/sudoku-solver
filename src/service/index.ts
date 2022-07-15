c30b4c2ce994ae5a3587d16f247b007fae62a9a6
OHFloree <2001.florian.mueller@gmail.com>
2022-07-15 17:57:38 +0200

    add parser to index

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,2 +1,5 @@
 export { default as ISolver } from "./solver/ISolver"
 export { default as Solver } from "./solver/BacktrackingSolver/BacktrackingSolver"
+
+export { default as IParser } from "./parser/IParser"
+export { default as Parser } from "./parser/Parser"
