a3f0b5f78870b5a3e2a688187c1ae8b2edc5276d
OHFloree <2001.florian.mueller@gmail.com>
2022-07-15 17:57:17 +0200

    updated controller

--- src/controller/solve-controller.ts
+++ src/controller/solve-controller.ts
@@ -1,16 +1,29 @@
 import { Request, Response } from "express"
+import * as O from "fp-ts/lib/Option"
+import { pipe, flow } from 'fp-ts/function'
 
+import { Solver, Parser } from "@/service"
 
 class SolveController {
-
+    parser = new Parser()
+    solver = new Solver()
 
     async get(req: Request, res: Response) {
-        const validationRegex = /^\d{9}(_\d{9}){8}$/
-        let isValid = validationRegex.test(req.params.puzzle)
-
-        res.json(isValid)
+        pipe(
+            req.params.puzzle,
+            (str) => this.parser.parse(str),
+            O.match(
+                () => { res.status(400).json("invalid input") },
+                flow(
+                    (puzzle => this.solver.solve(puzzle)),
+                    O.match(
+                        () => { res.status(400).json("no solution found") },
+                        (solution) => res.status(200).json(solution)
+                    )
+                )
+            )
+        )
     }
-
 }
 
 export default SolveController
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express"
 
 import SolveController from "@/controller/solve-controller"
+import { Parser, Solver } from "@/service"
 
 const router = Router()
 const solveController = new SolveController()
 
-router.get("/solve/:puzzle", solveController.get)
+router.get("/solve/:puzzle", (req, res) => solveController.get(req, res))
 
 export default router
